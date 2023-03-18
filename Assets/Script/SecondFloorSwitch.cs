ac573b89ad2f0d6161e20aaccc8f55d58065d1a2
wang <wang890921@gmail.com>
2023-03-18 21:15:04 +0800

    增加二樓及廁所場景 完善一樓觸發器及場景切換 一樓缺道具拾取及窗戶動畫

---
+++ Assets/Script/SecondFloorSwitch.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SecondFloorSwitch : MonoBehaviour
+{
+    public GameObject trig;
+    bool touched=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.trig.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow) | Input.GetKey("w")  && touched==true){
+            Debug.Log("進入二樓場景");
+            this.trig.SetActive(false);
+            SceneManager.LoadScene("SecondScene");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        this.trig.SetActive(true);
+        Debug.Log("碰到階梯了");
+        touched=true;
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        this.trig.SetActive(false);
+        touched=false;
+    }
+}
---
+++ Assets/Script/WindowController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowController : MonoBehaviour
+{
+    int count=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag=="Player"){
+            count++;
+            Debug.Log("第"+count+"次經過窗戶");
+        }
+        if(count==2){
+            Destroy(this.gameObject);
+        }
+    }
+}

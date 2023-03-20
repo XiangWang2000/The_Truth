0b923c7c106f05d196ab530c74e56d6b909f7ec1
Patric891228 <cchs.stu21047@gmail.com>
2023-03-20 16:51:02 +0800

    廁所戰鬥

---
+++ Assets/Script/ToiletScripts/DadFight.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DadFight : MonoBehaviour
+{   
+    public GameObject trig;
+    public Animation Anim;
+
+    bool touched=false;
+    float timer_f = 0f;
+    int timer_i = 0;
+    int timer_i_f = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.trig.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(Input.GetKeyDown(KeyCode.I)){
+            Anim.Play("SwitchFadeOut");
+        }
+        timer_f+=Time.deltaTime;
+        timer_i = (int)timer_f;
+        Debug.Log(timer_i);
+        if(Input.GetKey(KeyCode.F) && touched==true){
+            Debug.Log("輸入F了");
+            this.trig.SetActive(false);
+            StartCoroutine(ToFight());
+            Anim.Play("SwitchFadeOut");
+
+        }else if (touched==true){
+            if(timer_i-timer_i_f==2){
+                Debug.Log("沒輸F");
+                this.trig.SetActive(false);
+                StartCoroutine(ToDead());
+                Anim.Play("SwitchFadeOut");
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag=="Player"){
+            timer_i_f = timer_i;
+            this.trig.SetActive(true);
+            touched=true;
+            Debug.Log("碰到鬼了");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        this.trig.SetActive(false);
+        touched=false;
+    }
+    IEnumerator ToFight(){
+        yield return  new WaitForSeconds(2);
+        Application.LoadLevel("DadFightScene");
+    }
+    IEnumerator ToDead(){
+        yield return  new WaitForSeconds(2);
+        Application.LoadLevel("FirstScene");
+    }
+}

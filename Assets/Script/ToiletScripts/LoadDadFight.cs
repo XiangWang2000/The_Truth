8ae031c5003b6654ccb4d1aadaea8c191b44b23a
Patric891228 <cchs.stu21047@gmail.com>
2023-03-22 18:21:40 +0800

    新增攻擊選項

--- Assets/Script/ToiletScripts/LoadDadFight.cs
+++ Assets/Script/ToiletScripts/LoadDadFight.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadDadFight : MonoBehaviour
-{
+{      
+    public GameObject Btn1;
+    public GameObject Btn2;
+    public GameObject Btn3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int X1 = Random.Range(-100,100);
     }
 
     // Update is called once per frame

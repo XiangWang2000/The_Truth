1eafe0f2814a31abf0e894a6fe3f5127e1aaed9d
wang <wang890921@gmail.com>
2023-03-23 16:49:59 +0800

    增加死亡變數

--- Assets/Script/GameDataManager.cs
+++ Assets/Script/GameDataManager.cs
@@ -11,4 +11,12 @@ public static class GameDataManager
     public static bool Pot=false;
     public static bool move=true;
     public static int dead=0;
+    //1:位於時間內調查鬼
+    //2:道具不夠
+    //3:與鬼肉搏死亡
+    //4:使用繩子死亡
+    //5:使用鍋子死亡
+    //6:安撫死亡
+    //7:溝通死亡
+    //8:未靠近樓梯死亡
 }

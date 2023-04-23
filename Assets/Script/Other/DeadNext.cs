8eeb76c849925e738b1ce7678b138983f706c275
Patric891228 <cchs.stu21047@gmail.com>
2023-04-23 18:05:52 +0800

    弟弟進入戰鬥完成

--- Assets/Script/Other/DeadNext.cs
+++ Assets/Script/Other/DeadNext.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DeadNext : MonoBehaviour
-{
+{   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +13,15 @@ public class DeadNext : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {      
         if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log(GameDataManager.state);
+        }
+        if(Input.GetKeyDown(KeyCode.Space)&&GameDataManager.state==1){
             SceneManager.LoadScene("FirstScene");
+        }
+        else if(Input.GetKeyDown(KeyCode.Space)&&GameDataManager.state==2){
+            SceneManager.LoadScene("SecondScene");
         }
     }
 }

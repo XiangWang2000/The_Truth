8e25721c08bb25f5672b618c43e1adbb2c3e2bea
wang <wang890921@gmail.com>
2023-05-19 06:08:19 +0800

    再一次

--- Assets/Script/BacktoBrotherRoom.cs
+++ Assets/Script/BacktoBrotherRoom.cs
@@ -7,7 +7,7 @@ public class BacktoBrotherRoom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameDataManager.backfromabloodytissue = true;
+        GameDataManager.backfrombloodytissue = true;
     }
 
     // Update is called once per frame

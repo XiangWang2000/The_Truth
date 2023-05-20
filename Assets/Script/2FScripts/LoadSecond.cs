542f1aa3b8fc747196dbb6e47ac7634b4e175a1a
wang <wang890921@gmail.com>
2023-05-21 01:03:36 +0800

    修正部分場景選單中間圖片未抓取情況 修正二樓場景選單疊圖情況

--- Assets/Script/2FScripts/LoadSecond.cs
+++ Assets/Script/2FScripts/LoadSecond.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadSecond : MonoBehaviour
-{   
+{
     public GameObject GrandMa;
     public GameObject Player;
     public GameObject Dot1;
     public GameObject Dot2; //玩家移動一點阿罵現身
     public SpriteRenderer sr; // 奶奶的圖像
-    private bool isinGranadmaPart ;
+    private bool isinGranadmaPart;
     private bool isStartMove;
     // Start is called before the first frame update
     void Start()
-    {   
+    {
         isStartMove = false;
         this.isinGranadmaPart = GameDataManager.isinGranadmaPart;
         Debug.Log(isinGranadmaPart);
@@ -22,15 +22,18 @@ public class LoadSecond : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isinGranadmaPart && isStartMove){
-             Debug.Log("奶奶現身");
+        if (isinGranadmaPart && isStartMove)
+        {
+            Debug.Log("奶奶現身");
             GrandMa.SetActive(true);
         }
-        if(Player.transform.position.x > Dot1.transform.position.x ||Player.transform.position.x < Dot2.transform.position.x ){
+        if (Player.transform.position.x > Dot1.transform.position.x || Player.transform.position.x < Dot2.transform.position.x)
+        {
             isStartMove = true;
             Debug.Log("如果觸發奶奶事件，那麼奶奶該出現了");
         }
-        if(Player.transform.position.x==Dot1.transform.position.x){
+        if (Player.transform.position.x == Dot1.transform.position.x)
+        {
             sr.flipX = false;
         }
         // Debug.Log("玩家的位置"+Player.transform.position.x);
